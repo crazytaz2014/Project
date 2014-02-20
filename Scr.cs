e289e6e3e782962746b4595b40405a67d5f3091a
swapnil <crazytaz2014@gmail.com>
2014-02-20 13:39:42 -0600

    Adding Source code

---
+++ Scr.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace MyFirstProject.day1.examples
+{
+    class Example1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello! This my first project");
+           Console.ReadLine();
+        }
+    }
+}
